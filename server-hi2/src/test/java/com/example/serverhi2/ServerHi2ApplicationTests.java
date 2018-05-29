0c13e47fdf8d4232eeb317a1907cc84186751316
yinchong <yin.chong@intellif.com>
2018-05-29 21:28:12 +0800

    Initial commit

---
+++ server-hi2/src/test/java/com/example/serverhi2/ServerHi2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.serverhi2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerHi2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ service-hi/src/main/java/com/example/servicehi/ServiceHiApplication.java
@@ -0,0 +1,23 @@
+package com.example.servicehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi helloword";
+	}
+}
